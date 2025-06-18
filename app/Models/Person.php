18015f9fd8c6664c60fbef2263970f41e1fc3e67
marekl0605 <marekl0605@gmail.com>
2025-06-18 05:57:03 -0400

    fix: mass assignment protection error in creating people

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email', // add all other fields you want to allow
+    ];
 
     public function users()
     {
